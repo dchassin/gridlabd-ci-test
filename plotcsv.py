1817656d31070c0aceb00124d23ef1f2a8a81519
David P. Chassin <dchassin@slac.stanford.edu>
2019-04-17 14:11:28 -0700

    Update plotcsv.py

--- plotcsv.py
+++ plotcsv.py
@@ -7,9 +7,9 @@ def csvtopng(csvname,pngname=None,col=1,start=None,stop=None) :
 	data = {}
 	name = None
 	if type(start) == str :
-		start = datetime.datetime.strptime(start,"%Y-%m-%d %H:%M:%S %Z")
+		start = datetime.datetime.strptime(start,"%Y-%m-%d %H:%M:%S")
 	if type(stop) == str :
-		stop = datetime.datetime.strptime(stop,"%Y-%m-%d %H:%M:%S %Z")
+		stop = datetime.datetime.strptime(stop,"%Y-%m-%d %H:%M:%S")
 	with open("my_test.csv","r") as csvfile :
 		reader = csv.reader(csvfile);
 		for row in reader :
@@ -32,4 +32,4 @@ def csvtopng(csvname,pngname=None,col=1,start=None,stop=None) :
 	plt.savefig("my_test.png")
 
 if __name__ == '__main__':
-	csvtopng("my_test.csv",start="2019-01-01 00:00:00 PST",stop="2019-01-02 00:00:00 PST")
+	csvtopng("my_test.csv",start="2019-01-01 00:00:00",stop="2019-01-02 00:00:00")
