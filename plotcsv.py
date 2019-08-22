63cc9c308f9e9c9d31f1abd52d0ec7d672430ee3
David P. Chassin <dchassin@slac.stanford.edu>
2019-08-22 13:36:06 -0700

    Updated test

--- plotcsv.py
+++ plotcsv.py
@@ -1,35 +1,13 @@
-import datetime
-import csv
+import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
-def csvtopng(csvname,pngname=None,col=1,start=None,stop=None) :
-
-	data = {}
-	name = None
-	if type(start) == str :
-		start = datetime.datetime.strptime(start,"%Y-%m-%d %H:%M:%S")
-	if type(stop) == str :
-		stop = datetime.datetime.strptime(stop,"%Y-%m-%d %H:%M:%S")
-	with open("my_test.csv","r") as csvfile :
-		reader = csv.reader(csvfile);
-		for row in reader :
-			if row[0][0] == '#' :
-				if len(row) > col :
-					name = row[col]
-				continue
-			dt = datetime.datetime.strptime(row[0][0:19],"%Y-%m-%d %H:%M:%S")
-			if dt in data.keys() :
-				raise Exception("date/time '%s' duplicated" % row[0])
-			if ( start == None or dt >= start ) and ( stop == None or dt <= stop ) :
-				data[dt] = float(row[col].split(" ")[0])
-
-	plt.figure(1)
-	plt.plot(data.values())
-	if name != None :
-		plt.ylabel(name.split("[")[0].replace("_"," ").title())
-	plt.xlabel("Time")
-	plt.grid()
-	plt.savefig("my_test.png")
-
-if __name__ == '__main__':
-	csvtopng("my_test.csv",start="2019-01-01 00:00:00",stop="2019-01-02 00:00:00")
+data = pd.read_csv("my_test.csv",header=None,names=["Indoor"])
+plt.figure(1)
+data[-24*60:].plot()
+plt.xlabel("Date/Time")
+plt.xticks(rotation=90)
+plt.subplots_adjust(bottom=0.5)
+plt.grid()
+plt.ylabel("Air temperature [degF]")
+plt.savefig("my_test.png")
